065bc6344902602105d9b883059087a1bb88a8ff
Milena <mi_lky@mail.ru>
2023-05-12 12:42:51 +0300

    [feat] create and find all docs

--- app.js
+++ app.js
@@ -1,42 +1,68 @@
-const { MongoClient } = require('mongodb');
-const url = 'mongodb://127.0.0.1:27017';
-
-const client = new MongoClient(url, {
-  useUnifiedTopology: true,
+const { MongoClient, MongoKerberosError } = require('mongodb');
+const mongoose = require('mongoose');
+const DB_NAME = 'fruitsDB';
+mongoose.connect(`mongodb://127.0.0.1:27017/${DB_NAME}`, {
   useNewUrlParser: true,
 });
 
-run();
+console.log(`Successfully connected to db ${DB_NAME}`);
 
-async function run() {
-  try {
-    await client.connect();
-    const db = client.db('fruitsDB');
-    console.log(`Successfully connected to db ${db.databaseName}`);
-    await insertDocuments(db, function () {
-      client.close();
-    });
-    const cursor = db.collection('fruits').find({});
-    await cursor.forEach(console.dir);
-  } catch (err) {
-    console.error(`we encountered ${err}`);
-  } finally {
-    await client.close();
-  }
-}
+const fruitsSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
 
-const insertDocuments = async function (db, callback) {
-  const collection = db.collection('fruits');
+const Fruit = mongoose.model('Fruit', fruitsSchema);
 
-  const count = await collection.countDocuments();
-  if (count === 0) {
-    await collection.insertMany([
-      { name: 'Apple', score: 8, review: 'Great fruit' },
-      { name: 'Orange', score: 6, review: 'Kinda sour' },
-      { name: 'Banana', score: 9, review: 'Great stuff' },
-    ]);
-    console.log(`Records are inserted`);
-  } else {
-    console.log(`Records already exist`);
-  }
-};
+const fruit = new Fruit({
+  name: 'Apple',
+  rating: 8,
+  review: 'Great fruit',
+});
+
+// Uncomment the following code to save a fruit document to the database
+// fruit
+//   .save()
+//   .then(() => {
+//     console.log('Fruit saved successfully');
+//   })
+//   .catch((error) => {
+//     console.error('Error saving fruit:', error);
+//   });
+
+const banana = new Fruit({
+  name: 'Banana',
+  rating: 3,
+  review: 'Weird texture',
+});
+
+const orange = new Fruit({
+  name: 'Orange',
+  rating: 6,
+  review: 'Kinda sour',
+});
+
+// Uncomment the following code to insert multiple fruit documents to the database
+// Fruit.insertMany([banana, orange])
+//   .then((docs) => {
+//     const savedNames = docs.map((doc) => doc.name);
+//     const savedString = savedNames.join(', ');
+//     console.log(`${savedString} saved successfully`);
+//     mongoose.connection.close();
+//   })
+//   .catch((error) => {
+//     console.error('Error saving fruits:', error);
+//     mongoose.connection.close();
+//   });
+
+Fruit.find()
+  .then((fruits) => {
+    const savedNames = fruits.map((fruit) => fruit.name);
+    console.log('Found the following documents:');
+    console.log(savedNames);
+    mongoose.connection.close();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
